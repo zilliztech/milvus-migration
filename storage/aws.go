d1fc6c8f1e5e0f3e94c05f5c284ad8199882e943
lentitude2tk <xushuang.hu@zilliz.com>
2024-08-20 11:13:34 +0800

    support aws endpoint of china

--- storage/aws.go
+++ storage/aws.go
@@ -2,12 +2,14 @@ package storage
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/minio/minio-go/v7"
 	"github.com/minio/minio-go/v7/pkg/credentials"
 )
 
 const _awsEndpoint = "s3.amazonaws.com"
+const _awsChinaEndpoint = "s3.amazonaws.com.cn"
 
 func NewAWSClient(cfg Cfg) (*MinioClient, error) {
 	opts := minio.Options{Secure: cfg.UseSSL, Region: cfg.Region}
@@ -30,7 +32,12 @@ func NewAWSClient(cfg Cfg) (*MinioClient, error) {
 	if len(cfg.Endpoint) != 0 {
 		cli, err = minio.New(cfg.Endpoint, &opts)
 	} else {
-		cli, err = minio.New(_awsEndpoint, &opts)
+		isCNRegion := strings.HasPrefix(cfg.Region, "cn-")
+		if isCNRegion {
+			cli, err = minio.New(_awsChinaEndpoint, &opts)
+		} else {
+			cli, err = minio.New(_awsEndpoint, &opts)
+		}
 	}
 	if err != nil {
 		return nil, fmt.Errorf("storage: new aws client %w", err)
